00403ce3a6a62f5f3655cd69d63cafbdb712e6c2
Juliano Lorenzato <julianolorenzato@gmail.com>
2022-02-27 23:32:45 -0300

    m

--- src/api/sla/controllers/sla.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  sla controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::sla.sla');
--- src/api/sla/routes/sla.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * sla router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::sla.sla');
